8ed0634e4be43090e64afded40a4ee3bbc1e39e8
Nerahikada <hkshirorom@yahoo.co.jp>
2021-08-11 23:57:44 +0900

    PlayerがLanguageを持つように

--- src/NeiroNetwork/TranslationPlugin/pocketmine/Player.php
+++ src/NeiroNetwork/TranslationPlugin/pocketmine/Player.php
@@ -5,12 +5,23 @@ declare(strict_types=1);
 namespace NeiroNetwork\TranslationPlugin\pocketmine;
 
 use NeiroNetwork\TranslationPlugin\LanguageFactory;
-use pocketmine\lang\Language;
+use pocketmine\entity\Location;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\NetworkSession;
 use pocketmine\player\Player as PmPlayer;
+use pocketmine\player\PlayerInfo;
+use pocketmine\Server;
 
 class Player extends PmPlayer{
 
+	private Language $language;
+
+	public function __construct(Server $server, NetworkSession $session, PlayerInfo $playerInfo, bool $authenticated, Location $spawnLocation, ?CompoundTag $namedtag){
+		parent::__construct($server, $session, $playerInfo, $authenticated, $spawnLocation, $namedtag);
+		$this->language = LanguageFactory::getInstance()->get($this->getLocale());
+	}
+
 	public function getLanguage() : Language{
-		return LanguageFactory::getInstance()->get($this->getLocale());
+		return $this->language;
 	}
 }
