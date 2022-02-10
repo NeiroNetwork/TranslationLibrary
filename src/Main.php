17b260c79f42621b36f0027ce2787353b3fe022f
Nerahikada <hkshirorom@yahoo.co.jp>
2022-02-10 14:56:47 +0900

    よりシンプルに作り直し

--- src/Main.php
+++ src/Main.php
@@ -5,31 +5,5 @@ declare(strict_types=1);
 namespace NeiroNetwork\TranslationPlugin;
 
 use pocketmine\plugin\PluginBase;
-use ReflectionClass;
 
-class Main extends PluginBase{
-
-	protected function onEnable() : void{
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
-
-		foreach($this->getServer()->getPluginManager()->getPlugins() as $plugin){
-			$hasTranslation = false;
-			foreach($plugin->getResources() as $file){
-				if($file->getExtension() === "ini"){
-					$code = $file->getBasename(".{$file->getExtension()}");
-					LanguageFactory::getInstance()->get($code)->addLang($file->getPath(), $code);
-					$hasTranslation = $file->getPath();
-				}
-			}
-			if($hasTranslation){
-				foreach(LanguageFactory::getInstance()->getAllRegistered() as $language){
-					$language->addFallbackLang($hasTranslation);
-				}
-			}
-		}
-
-		$property = (new ReflectionClass($this->getServer()))->getProperty("forceLanguage");
-		$property->setAccessible(true);
-		$property->setValue($this->getServer(), true);
-	}
-}
+class Main extends PluginBase{}
---
+++ src/Translator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\TranslationPlugin;
+
+use pocketmine\lang\Language;
+use pocketmine\lang\LanguageNotFoundException;
+use pocketmine\lang\Translatable;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Translator{
+
+	/** @var Language[] */
+	protected array $languages = [];
+
+	public function __construct(PluginBase $plugin, protected string $baseLocale = "ja_jp"){
+		foreach($plugin->getResources() as $info){
+			$name = $info->getBasename("." . $ext = $info->getExtension());
+			if($ext === "ini" && preg_match("/[a-z]{2}_[a-z]{2}/", $name) === 1){
+				$this->languages[$name] = new Language($name, $info->getPath(), $baseLocale);
+			}
+		}
+
+		if(!isset($this->languages[$baseLocale])){
+			throw new LanguageNotFoundException("Base locale \"$baseLocale\" not found");
+		}
+	}
+
+	public function translate(Translatable $translatable, ?Player $player = null) : string{
+		$language = $this->languages[$player?->getLocale()] ?? $this->languages[$this->baseLocale];
+		return $language->translate($translatable);
+	}
+
+	public function t(Translatable $translatable, ?Player $player = null) : string{
+		return $this->translate($translatable, $player);
+	}
+}
